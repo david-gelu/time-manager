45cd263d196f4778be275475dceb0ddba37a18bf
david-gelu <david.gelu90@gmail.com>
2025-10-08 18:00:08 +0300

    fixes for build vercel

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig(({ mode }) => {
         "@": path.resolve(__dirname, "./src"),
       },
     },
+    build: {
+      outDir: 'dist'
+    },
     server: {
       port: 5173,
       proxy: {
